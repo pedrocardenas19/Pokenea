bea9afe24674654f2637426829698d9390fa533a
JuanJmf01 <juaneltrozo13@gmail.com>
2024-05-31 15:45:41 -0500

    models.py updated.

--- app/models.py
+++ app/models.py
@@ -8,11 +8,11 @@ class Pokenea:
         self.frase = frase
 
 POKENEAS = [
-    Pokenea(1, 'Pokenea1', 1.2, 'Habilidad1', 'https://storage.googleapis.com/bucket/pokenea1.jpg', 'Frase filosófica 1'),
-    Pokenea(2, 'Pokenea2', 1.5, 'Habilidad2', 'https://storage.googleapis.com/bucket/pokenea2.jpg', 'Frase filosófica 2'),
-    Pokenea(3, 'Pokenea3', 1.3, 'Habilidad3', 'https://storage.googleapis.com/bucket/pokenea3.jpg', 'Frase filosófica 3'),
-    Pokenea(4, 'Pokenea4', 1.4, 'Habilidad4', 'https://storage.googleapis.com/bucket/pokenea4.jpg', 'Frase filosófica 4'),
-    Pokenea(5, 'Pokenea5', 1.1, 'Habilidad5', 'https://storage.googleapis.com/bucket/pokenea5.jpg', 'Frase filosófica 5'),
-    Pokenea(6, 'Pokenea6', 1.6, 'Habilidad6', 'https://storage.googleapis.com/bucket/pokenea6.jpg', 'Frase filosófica 6'),
-    Pokenea(7, 'Pokenea7', 1.7, 'Habilidad7', 'https://storage.googleapis.com/bucket/pokenea7.jpg', 'Frase filosófica 7'),
+    Pokenea(1, 'Pokenea1', 1.2, 'Confía en el Señor con todo tu corazón." - Proverbios 3:5', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/1.png?alt=media&token=c7322049-977b-47ee-94f3-e7bcf65ceca8', 'Confía en el Señor con todo tu corazón." - Proverbios 3:5'),
+    Pokenea(2, 'Pokenea2', 1.5, 'Marte nos protege" - Braulio y theGaff', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/2.jpg?alt=media&token=507e8ae0-4671-4b58-89b2-f061b4803d37', 'Confía en el Señor con todo tu corazón." - Proverbios 3:5'),
+    Pokenea(3, 'Pokenea3', 1.3, 'Pedid, y se os dará." - Mateo 7:7', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/3.png?alt=media&token=7b06dc25-682d-4592-8938-4cfd54dbd6f1', 'Pedid, y se os dará." - Mateo 7:7'),
+    Pokenea(4, 'Pokenea4', 1.4, 'La paz os dejo, mi paz os doy." - Juan 14:27', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/4.jpg?alt=media&token=dabb1735-616c-4bf1-8025-38a37c5ad7f6', 'La paz os dejo, mi paz os doy." - Juan 14:27'),
+    Pokenea(5, 'Pokenea5', 1.1, 'En el principio creó Dios los cielos y la tierra', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/5.png?alt=media&token=01a76405-636a-4e8b-bc7f-39f411d8c14c', 'En el principio creó Dios los cielos y la tierra." - Génesis 1:1'),
+    Pokenea(6, 'Pokenea6', 1.6, 'Porque para Dios no hay nada imposible." - Lucas 1:37', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/6.png?alt=media&token=53d49a86-63e8-4101-bbbb-410fb91f9fc7', 'Porque para Dios no hay nada imposible." - Lucas 1:37'),
+    Pokenea(7, 'Pokenea7', 1.7, 'Yo soy el camino, la verdad y la vida. Nadie viene al Padre sino por mí." - Juan 14:6', 'https://firebasestorage.googleapis.com/v0/b/entrega2-6bee0.appspot.com/o/7.jpg?alt=media&token=f837ee45-5983-4591-9ea7-c4c6743ae6d7', 'Yo soy el camino, la verdad y la vida. Nadie viene al Padre sino por mí." - Juan 14:6'),
 ]
